20eaf20631cacd8b0f5bb5152ef9fda5e134ff70
fpozzana <49064037+fpozzana@users.noreply.github.com>
2020-12-06 11:18:24 +0100

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -1,4 +1,4 @@
-module UART_top
+module UART_top #(parameter clk_freq_Hz = 1000000)
 (
   input logic               clk,
   input logic               RSTn,
@@ -10,3 +10,29 @@ module UART_top
   input logic               RX,
   output logic              TX
 );
+
+//debouncer + one shot for start button in
+logic button_debounced, one_shot_out;
+
+debouncer #(clk_freq_Hz)
+debouncer (.clk(clk),
+                     .RSTn(RSTn),
+                     .button_in(start),
+                     .button_out(button_debounced)
+);
+
+one_shot one_shot (.clk(clk),
+                   .RSTn(RSTn),
+                   .sig_in(button_debounced),
+                   .sig_out(one_shot_out)
+);
+
+//TX fsm
+tx_fsm tx_fsm (.clk(clk),
+               .RSTn(RSTn),
+               .start(one_shot_out),
+               .data_in(data_in),
+               .TX(TX)
+);
+
+endmodule
